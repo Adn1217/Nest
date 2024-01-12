6f0c97ebe0a6da1b75ea56ebf63c7bbd94966716
Adrian Fernandez <77861592+Adn1217@users.noreply.github.com>
2024-01-12 11:55:03 -0500

    First version of users CRUD

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,9 +1,8 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { StudentsService } from './students.service';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { StudentsService, newUser } from './students.service';
 
 @Controller('students')
 export class StudentsController {
-
 
   constructor( private readonly studentsService: StudentsService){
 
@@ -15,11 +14,26 @@ export class StudentsController {
     return students
   }
 
-  @Get(':id')
-  getStudentById(@Param('id') id: string){
-
+  @Get('/:id')
+  getStudentById(@Param('id', ParseIntPipe) id: number){
     const student = this.studentsService.findById(+id);
-    
     return student 
+  }
+
+  @Post()
+  createStudent(@Body() body: newUser){
+    return body
+  }
+  
+  @Patch('/:id')
+  updateStudent(@Param('id', ParseIntPipe) id: number, @Body() body: newUser){
+    return body
+  }
+
+  @Delete('/:id')
+  deleteStudent(@Param('id', ParseIntPipe) id: number){
+    return {
+      message: `Eliminado exitoso del usuario ${id}`
+    }
   }
 }
--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,35 +1,93 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+export type userRol = 'user' | 'admin' | null
 
+export interface newUser {
+    nombres: string;
+    apellidos: string;
+    usuario: string;
+    edad: number;
+    correo: string;
+    password: string;
+    role: userRol
+  }
+export interface users extends newUser {
+    id: number;
+  }
 @Injectable()
 export class StudentsService {
 
-  private students = [
-    {
-        id: 1,
-        name: 'A'
-    },
-    {
-        id:2,
-        name: 'B'
-    },
-    {
-        id:3,
-        name: 'C'
-    }
-    ]
+    private students : users [] = [
+          {
+            "id": 1,
+            "nombres": "Adrian Alberto",
+            "apellidos": "Fernández Cabrera",
+            "usuario": "adn1217",
+            "edad": 32,
+            "correo": "adn1217@hotmail.com",
+            "password": "12345678",
+            "role": "admin"
+          },
+          {
+            "id": 2,
+            "nombres": "Alejandra Paola",
+            "apellidos": "Fernández Castro",
+            "usuario": "alu2110",
+            "edad": 31,
+            "correo": "alufndz_@gmail.com",
+            "password": "12345678",
+            "role": "user"
+          },
+          {
+            "id": 3,
+            "nombres": "Rupertico Adolfo",
+            "apellidos": "Herrera Gonzalez",
+            "usuario": "ruper12",
+            "edad": 33,
+            "correo": "raherreraG@gmail.com",
+            "password": "12345678",
+            "role": "user"
+          },
+          {
+            "id": 1693702085020,
+            "nombres": "Keyner Antonio",
+            "apellidos": "Fuentes Fontalvo",
+            "usuario": "adn1217",
+            "edad": 22,
+            "correo": "adn1217@hotmail.com",
+            "password": "11111111",
+            "role": "user"
+          },
+          {
+            "id": 1693794786589,
+            "nombres": "Adrian Alberto",
+            "apellidos": "Fernández Castro",
+            "usuario": "adn1217",
+            "edad": 23,
+            "correo": "adn1219@hotmail.com",
+            "password": "11111111",
+            "role": "user"
+          },
+          {
+            "id": 1700950786402,
+            "nombres": "Esmeregildo",
+            "apellidos": "Segrera fuentes",
+            "usuario": "esmese",
+            "edad": 67,
+            "correo": "esme.segrera@hotmail.com",
+            "password": "12345678",
+            "role": "user"
+          }
+        ]
 
 
   findAll(){
     return this.students;
   }
 
-  findById(id: number){
-    let student = this.students.find((student) => student.id === id);
+  findById(id: number): users | undefined{
+    const student = this.students.find((student) => student.id === id);
     if(!student){
-        student = {
-            id: id,
-            name: 'No encontrado' 
-        }
+        throw new NotFoundException(`Usuario con id: ${id} no encontrado.`);
     }
     return student
   }
