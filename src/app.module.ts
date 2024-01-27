a68a1cfb8c5da06185bd8db27d0b6b350a789dc0
Adrian Fernandez <77861592+Adn1217@users.noreply.github.com>
2024-01-27 17:05:10 -0500

    Middleware to valide mongoId is implemented in Courses endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentsModule } from './students/students.module';
@@ -7,10 +7,26 @@ import { SeedModule } from './seed/seed.module';
 import { TeachersModule } from './teachers/teachers.module';
 import { EnrollmentsModule } from './enrollments/enrollments.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { isMongoId } from './common/middlewares/isMongoId.middeware';
+import { CoursesController } from './courses/courses.controller';
 
 @Module({
   imports: [StudentsModule, CoursesModule, SeedModule, TeachersModule, EnrollmentsModule, MongooseModule.forRoot('mongodb://localhost:27017/nest-academy-platform')],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer){
+    consumer.apply(isMongoId)
+            .exclude(
+              { 
+                path: 'courses',
+                method: RequestMethod.GET,
+              },
+              {
+              path: 'courses', method: RequestMethod.POST
+              }
+            )
+            .forRoutes(CoursesController)
+  }
+}
